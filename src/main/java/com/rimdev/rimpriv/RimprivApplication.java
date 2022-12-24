009b699c3030909cbb7ce8d46fd8a61f96ee863d
guardianpro <ahmed.rimdev@gmail.com>
2022-12-24 10:55:22 +0200

    adding project init

---
+++ src/main/java/com/rimdev/rimpriv/RimprivApplication.java
@@ -0,0 +1,13 @@
+package com.rimdev.rimpriv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimprivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimprivApplication.class, args);
+	}
+
+}
